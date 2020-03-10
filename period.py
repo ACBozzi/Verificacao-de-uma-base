d3695018f4b9a03e0c10470f87994ed55bdc6cdb
ACBozzi <annac.bozzi@gmail.com>
2020-03-10 11:27:03 -0300

    2

--- period.py
+++ period.py
@@ -2,20 +2,22 @@
 
 from datetime import datetime
 from itertools import dropwhile , takewhile
-
+from datetime import date
 import instaloader
+
 
 #pega a instancia do instaloader
 L = instaloader . Instaloader ()
 
 posts = L . get_hashtag_posts ( 'deolhonoscorais' )
+desde = datetime ( 2018 , 1 , 2 )	#data maior
+ate = datetime ( 2017 , 1 , 1 )	#data menor
 
-desde = datetime ( 2019 , 12 , 1 )	#data maior
-ate = datetime ( 2018 , 5 , 1 )	#data menor
 
 #lambda usa para 'variável' desconhecida
 #takewhile itera desde que o predicado seja verdadeiro
 #dropwhile elimina do iterável desde que o predicado seja verdadeiro
 for post in takewhile ( lambda p : p . date > ate , dropwhile ( lambda p : p . date > desde , posts )):
-    print ( post . date )
-    L . download_post ( post , '#deolhonoscorais' )
+   	print ( post . date )
+   	L . download_post ( post , '#deolhonoscorais' )
+
