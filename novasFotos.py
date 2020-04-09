012e7faf06a3c85b6ff515d2db5fc68abf872797
“ACBozzi” <annac.bozzi@gmail.com>
2020-04-09 11:49:09 -0300

    dando erro para mais de uma foto na mesma publicação

--- novasFotos.py
+++ novasFotos.py
@@ -6,7 +6,7 @@ arq = open("teste.txt","w")
 primeira = 1
 
 for line in lines:
-	if primeira!=1 and primeira !=(len(lines)) and primeira != (len(lines)-1):
+	if primeira!=1:	#o instaloader printa uma primeira linha avisando que esta realizando o dowload das fotos
 		if "exists" in line:
 			print('')
 		else:
@@ -20,10 +20,11 @@ arq.close()
 arq = open("teste.txt","w")
 primeira = 1
 
+#instaloader printa as duas ultimas linhas com informações de encerramento
 for line in lines:
 	if primeira !=(len(lines)) and primeira != (len(lines)-1):
-		arq.write(line)
+		lista = line.split("/")[1] 	#separar apenas o nome
+		arq.write(lista)
 	primeira+=1
-
 
 arq.close()
