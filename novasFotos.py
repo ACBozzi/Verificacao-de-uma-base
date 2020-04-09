70f647c652565ca62f8db11a6b729543caa80c7a
“ACBozzi” <annac.bozzi@gmail.com>
2020-04-08 22:55:48 -0300

    ok

--- novasFotos.py
+++ novasFotos.py
@@ -3,12 +3,27 @@ lines = arq.readlines()
 arq.close()
 
 arq = open("teste.txt","w")
-
+primeira = 1
 
 for line in lines:
-	if "exists" in line:
-		print('')
-	else:
+	if primeira!=1 and primeira !=(len(lines)) and primeira != (len(lines)-1):
+		if "exists" in line:
+			print('')
+		else:
+			arq.write(line)
+	primeira+=1
+
+arq = open("teste.txt","r")
+lines = arq.readlines()
+arq.close()
+
+arq = open("teste.txt","w")
+primeira = 1
+
+for line in lines:
+	if primeira !=(len(lines)) and primeira != (len(lines)-1):
 		arq.write(line)
+	primeira+=1
+
 
 arq.close()
