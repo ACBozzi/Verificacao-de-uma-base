e77b393f0b4e6adb892dca4dc605124e04bc9aeb
“ACBozzi” <annac.bozzi@gmail.com>
2020-07-24 16:58:47 -0300

    baixa os dados do dia atual até 30 dias antes

---
+++ period.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from itertools import dropwhile, takewhile
+from datetime import date
+import instaloader
+import dateutil.relativedelta
+
+L = instaloader.Instaloader()
+
+posts = L.get_hashtag_posts('deolhonoscorais')
+SINCE = datetime.now() #data de hoje
+UNTIL = SINCE - dateutil.relativedelta.relativedelta(months=4) #um mês antes
+
+print(type(UNTIL))
+for post in takewhile(lambda p: p.date > UNTIL, dropwhile(lambda p: p.date > SINCE, posts)):
+    print(post.date)
+    L.download_post(post, "#deolhonoscorais")
