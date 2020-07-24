55e8ef2eaab1d14422e1f71cd35fdee0c714a401
“ACBozzi” <annac.bozzi@gmail.com>
2020-07-24 17:17:21 -0300

    baixa os dados de hoje até 30 dias antes

--- period.py
+++ period.py
@@ -10,7 +10,6 @@ posts = L.get_hashtag_posts('deolhonoscorais')
 SINCE = datetime.now() #data de hoje
 UNTIL = SINCE - dateutil.relativedelta.relativedelta(months=4) #um mês antes
 
-print(type(UNTIL))
 for post in takewhile(lambda p: p.date > UNTIL, dropwhile(lambda p: p.date > SINCE, posts)):
     print(post.date)
     L.download_post(post, "#deolhonoscorais")
